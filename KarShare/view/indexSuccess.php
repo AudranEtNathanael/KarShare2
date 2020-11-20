1bc7f9a12770d6aee45a488dd6781bd7468223fb
Nathanael-Lefevre <nathanael.lefevre@alumni.univ-avignon.fr>
2020-11-20 15:14:44 +0100

    test Audran rechercher voyage nom correction dans indesx

--- KarShare/view/indexSuccess.php
+++ KarShare/view/indexSuccess.php
@@ -4,7 +4,7 @@ C'est l'action par défaut !
 
 <form action="monApplication.php" target="_blank" method="get" style="width:50%">
 <div class="w3-row w3-center">
-<input type="hidden" name="action" value="RechercherVoyage">
+<input type="hidden" name="action" value="rechercherVoyage">
 <input type="submit" value="RechercherVoyage" class="w3-col l6 w3-button w3-theme-d3">
 <input type="text" name="depart" value="Paris" class="w3-col l6 w3-input w3-border">
 <input type="text" name="arrivee" value="Marseille" class="w3-col l6 w3-input w3-border">
